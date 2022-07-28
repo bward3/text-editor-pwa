af18e84355405c7197765ac1c8b95d419c294b5f
Brian Ward <brianwardcs@gmail.com>
2022-07-28 15:25:46 -0600

    Pushing to heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,55 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("Post to db");
+
+  // open connection to jate DB
+  const jateDB = await openDB("jate", 1);
+
+  // create transaction with database
+  const tx = jateDB.transaction("jate", "readwrite");
+
+  // open object store we want to add to
+  const store = tx.objectStore("jate");
+
+  const req = store.put({ content });
+
+  // await confirmation for the request
+  const res = await req;
+  console.log('Data successfully saved to DB!');
+  // return res;
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from DB');
+
+  // open connection to db
+  const jateDB = await openDB('jate', 1);
+
+  // create transaction. only need to read for this request
+  const tx = await jateDB.transaction('jate', 'readonly');
+
+  // open object store
+  const store = tx.objectStore('jate');
+
+  // getAll() data in db
+  const req = store.getAll();
+
+  const res = await req;
+  return res;
+};
 
 initdb();
